dc7d0eed0192a1bee973b41b6328d497e52c343d
le2sky <leehaneul990623@gmail.com>
2023-07-04 07:07:48 +0900

    test : @Mock, @Spy, @InjectMocks 사용해보기

--- src/main/java/sample/cafekiosk/spring/client/MailSendClient.java
+++ src/main/java/sample/cafekiosk/spring/client/MailSendClient.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class MailSendClient {
+
     public boolean sendEmail(String fromEmail, String toEmail, String subject, String content) {
         log.info("메일 전송");
         throw new IllegalArgumentException("메일 전송");
