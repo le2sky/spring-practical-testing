a346c377c28e4a9b0acc03fd20520378d42f7e75
le2sky <leehaneul990623@gmail.com>
2023-07-05 02:33:50 +0900

    refactor : 테스트 환경 통합하기

--- src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.domain.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,5 +12,5 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("select o from Order o where o.registeredDateTime >= :startDateTime " +
             "and o.registeredDateTime < :endDateTime " +
             "and o.status = :status")
-    List<Order> findOrdersBy(LocalDateTime startDateTime, LocalDateTime endDateTime, OrderStatus status);
+    List<Order> findOrdersBy(@Param("startDateTime") LocalDateTime startDateTime, @Param("endDateTime") LocalDateTime endDateTime, @Param("status") OrderStatus status);
 }
