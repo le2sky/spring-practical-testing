1ad14634646826338695df428d7e81a6c1c0f6b5
le2sky <leehaneul990623@gmail.com>
2023-07-05 01:49:28 +0900

    refactor : parameterized test 적용

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -2,6 +2,12 @@ package sample.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,15 +27,33 @@ class ProductTypeTest {
     }
 
     @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
-    @Test
-    void containsStockType2() {
-        // given
-        ProductType givenType = ProductType.BAKERY;
-
+    @CsvSource({"HANDMADE,false", "BOTTLE,true", "BAKERY,true"})
+    @ParameterizedTest
+    void containsStockTypeWithParameterizedTest(ProductType productType, boolean expected) {
         // when
-        boolean result = ProductType.containsStockType(givenType);
+        boolean result = ProductType.containsStockType(productType);
 
         // then
-        assertThat(result).isTrue();
+        assertThat(result).isEqualTo(expected);
+    }
+
+    // given이라서 위에 테스트 코드 위에 작성
+    private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @MethodSource("provideProductTypesForCheckingStockType")
+    @ParameterizedTest
+    void containsStockTypeWithMethodSource(ProductType productType, boolean expected) {
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
